35ab7c54cba4245e11cec93041e717f6cb03f2bc
Marin Atanasov <tyxla@abv.bg>
2014-11-27 22:51:35 +0200

    adding word-character-count module template

---
+++ templates/module-word-character-count.php
@@ -0,0 +1,32 @@
+<div class="word-character-count">
+	<table>
+		<tr>
+			<th>Words</th>
+			<td class="wcc-words"></td>
+		</tr>
+		<tr>
+			<th>Characters</th>
+			<td class="wcc-characters"></td>
+		</tr>
+		<tr>
+			<th>Characters (no spaces)</th>
+			<td class="wcc-characters-no-spaces"></td>
+		</tr>
+		<tr>
+			<th>Sentences</th>
+			<td class="wcc-sentences"></td>
+		</tr>
+		<tr>
+			<th>Average Sentence Words</th>
+			<td class="wcc-avg-sentence-words"></td>
+		</tr>
+		<tr>
+			<th>Average Sentence Characters</th>
+			<td class="wcc-avg-sentence-characters"></td>
+		</tr>
+		<tr>
+			<th>Paragraphs</th>
+			<td class="wcc-paragraphs"></td>
+		</tr>
+	</table>
+</div>
