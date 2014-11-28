410058062e47383774eee200b8c56c84d26d7d4e
Marin Atanasov <tyxla@abv.bg>
2014-11-28 23:49:17 +0200

    adding some comments to word-character-count js

--- modules/word-character-count/js/module.js
+++ modules/word-character-count/js/module.js
@@ -1,3 +1,5 @@
+// Main WP_Linguist.Module_Word_Character_Count model 
+
 (function($){
 	WP_Linguist.Module_Word_Character_Count = WP_Linguist_Module.extend({
 
--- modules/word-character-count/js/view.js
+++ modules/word-character-count/js/view.js
@@ -1,3 +1,5 @@
+// Main WP_Linguist.Module_Word_Character_Count view
+
 (function($){
 	WP_Linguist.Module_Word_Character_Count_View = WP_Linguist_Module_View.extend({
 		
