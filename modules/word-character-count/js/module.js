695fe3fa3cbad0556d8095a81e6cc69744f255fc
Marin Atanasov <tyxla@abv.bg>
2014-11-28 23:57:57 +0200

    adding some comments to word-character-count js

--- modules/word-character-count/js/module.js
+++ modules/word-character-count/js/module.js
@@ -1,4 +1,4 @@
-// Main WP_Linguist.Module_Word_Character_Count model 
+// Main WP_Linguist.Module.Word_Character_Count model 
 
 (function($){
 	WP_Linguist.Module.Word_Character_Count = WP_Linguist.Module.extend({
