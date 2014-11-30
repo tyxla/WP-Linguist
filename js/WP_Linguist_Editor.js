cd4c85431f297280f17efec8f88c8284e71be714
Marin Atanasov <tyxla@abv.bg>
2014-11-30 23:23:20 +0200

    wip word & character module

--- js/WP_Linguist_Editor.js
+++ js/WP_Linguist_Editor.js
@@ -5,7 +5,10 @@
 
 		initialize: function() {
 			var obj = this;
+
 			$(window).on('load', function() {
+
+				// call our update method when the tinyMCE main editor is updated
 				obj.instance = tinyMCE.get('content');
 				obj.instance.on('change', function(e) {
 					obj.update( this.getContent() );
@@ -14,6 +17,9 @@
 		},
 
 		update: function(content) {
+			// when main tinyMCE editor is updated, update the main object
+			// which will consecutively update all module models
+			// and respectively, their views
 			wp_linguist.update(content);
 		}
 
--- modules/word-character-count/js/view.js
+++ modules/word-character-count/js/view.js
@@ -4,6 +4,7 @@
 	WP_Linguist.Module_View.Word_Character_Count = WP_Linguist.Module_View.extend({
 		
 		initialize: function() {
+			// make sure view is updated on module update
 			this.model.on('change', this.render, this);
 		},
 
