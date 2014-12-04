66215c6fe780f7f8645ac0145879ca06153394e2
Marin Atanasov <tyxla@abv.bg>
2014-12-04 23:35:52 +0200

    word & character count module template - localization

--- templates/module-word-character-count.php
+++ templates/module-word-character-count.php
@@ -1,31 +1,31 @@
 <div id="wp-linguist-module-wcc" class="word-character-count">
 	<table>
 		<tr>
-			<th>Words:</th>
+			<th><?php _e('Words:', 'wp_linguist'); ?></th>
 			<td><span id="wp-linguist-module-wcc-words"></span></td>
 		</tr>
 		<tr>
-			<th>Characters:</th>
+			<th><?php _e('Characters:', 'wp_linguist'); ?></th>
 			<td><span id="wp-linguist-module-wcc-characters"></span></td>
 		</tr>
 		<tr>
-			<th>Characters (no spaces):</th>
+			<th><?php _e('Characters (no spaces):', 'wp_linguist'); ?></th>
 			<td><span id="wp-linguist-module-wcc-characters_no_spaces"></span></td>
 		</tr>
 		<tr>
-			<th>Sentences:</th>
+			<th><?php _e('Sentences:', 'wp_linguist'); ?></th>
 			<td><span id="wp-linguist-module-wcc-sentences"></span></td>
 		</tr>
 		<tr>
-			<th>Average Sentence Words:</th>
+			<th><?php _e('Average Sentence Words:', 'wp_linguist'); ?></th>
 			<td><span id="wp-linguist-module-wcc-avg_sentence_words"></span></td>
 		</tr>
 		<tr>
-			<th>Average Sentence Characters:</th>
+			<th><?php _e('Average Sentence Characters:', 'wp_linguist'); ?></th>
 			<td><span id="wp-linguist-module-wcc-avg_sentence_characters"></span></td>
 		</tr>
 		<tr>
-			<th>Paragraphs:</th>
+			<th><?php _e('Paragraphs:', 'wp_linguist'); ?></th>
 			<td><span id="wp-linguist-module-wcc-paragraphs"></span></td>
 		</tr>
 	</table>
