6ed67ed3544b3cae9d9a39b03801b1df8dd5bb9b
Marin Atanasov <tyxla@abv.bg>
2014-12-04 23:29:01 +0200

    wp_linguist_clean()

--- js/main.js
+++ js/main.js
@@ -6,3 +6,14 @@ var wp_linguist;
 	wp_linguist = new WP_Linguist();
 	
 })(jQuery);
+
+// Clean array from empty values
+Array.prototype.wp_linguist_clean = function() {
+	for (var i = 0; i < this.length; i++) {
+		if (this[i] == "") {         
+			this.splice(i, 1);
+			i--;
+		}
+	}
+	return this;
+};
